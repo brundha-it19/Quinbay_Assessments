14ac21676d009976bea822232b6f62414086155a
Brundha B <53815867+brundha-it19@users.noreply.github.com>
2023-01-17 13:46:47 +0530

    Add files via upload

---
+++ DAY4/App.vue
@@ -0,0 +1,29 @@
+<template>
+  
+  <header_box/>   
+</template>
+
+<script>
+
+import header_box from './components/header_box.vue'
+export default {     
+  name: 'App',
+  components: { 
+    header_box
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
+
