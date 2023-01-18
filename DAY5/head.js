774413602f11bb643825368400bedd897489ff99
Brundha B <53815867+brundha-it19@users.noreply.github.com>
2023-01-18 13:36:08 +0530

    Add files via upload

---
+++ DAY5/head.js
@@ -0,0 +1,206 @@
+
+export default{
+    data()
+    {
+        return{
+            title:'Products List',
+            filters:'Filter section',  
+            filt_table: [
+                { 
+                    mobilename:"Oppo f11 pro",
+                    brand:"oppo",
+                    price:"23000",
+                    ram:"8",
+                    Storage:"64",
+                    image:"http://reliabledukan.com/wp-content/uploads/2020/07/Oppo-F11-pro-matte-2.jpeg"
+                },
+                {
+                    mobilename:"Vivo y16",
+                    brand:"vivo",
+                    price:"28000",
+                    ram:"8",
+                    Storage:"64",
+                    image:"https://fdn2.mobgsm.com/vv/bigpic/oppo-f11-pro-.jpg"
+                },
+                {
+                    mobilename:"samsung s20",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"128",
+                    image:"https://m.media-amazon.com/images/I/71RxOftVoQL._SL1500_.jpg"
+                },
+                {
+                    mobilename:"vivo y19",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"256",
+                    image:"https://in-exstatic-vivofs.vivo.com/gdHFRinHEMrj3yPG/1572868801146/cc4fe54a5e9b44c5a1b28c44bb309ccc.png"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"poco",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"32",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"samsung galaxy s21",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"poco M5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://s3b.cashify.in/gpro/uploads/2021/12/06101415/poco-m5-front-2.jpg"
+                    
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"32",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"Realme c33",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"128",
+                    image:"https://cdn1.smartprix.com/rx-iIa55V4wF-w1200-h1200/Ia55V4wF.jpg"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                }
+            ],
+            
+            Products:[
+                { 
+                    mobilename:"Oppo f11 pro",
+                    brand:"oppo",
+                    price:"23000",
+                    ram:"8",
+                    Storage:"64",
+                    image:"http://reliabledukan.com/wp-content/uploads/2020/07/Oppo-F11-pro-matte-2.jpeg"
+                },
+                {
+                    mobilename:"Vivo y16",
+                    brand:"vivo",
+                    price:"28000",
+                    ram:"8",
+                    Storage:"64",
+                    image:"https://fdn2.mobgsm.com/vv/bigpic/oppo-f11-pro-.jpg"
+                },
+                {
+                    mobilename:"samsung s20",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"128",
+                    image:"https://m.media-amazon.com/images/I/71RxOftVoQL._SL1500_.jpg"
+                },
+                {
+                    mobilename:"vivo y19",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"256",
+                    image:"https://in-exstatic-vivofs.vivo.com/gdHFRinHEMrj3yPG/1572868801146/cc4fe54a5e9b44c5a1b28c44bb309ccc.png"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"poco",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"32",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"samsung galaxy s21",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"poco M5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://s3b.cashify.in/gpro/uploads/2021/12/06101415/poco-m5-front-2.jpg"
+                },
+                {
+                    mobilename:"Realme c33",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"32",
+                    image:"https://cdn1.smartprix.com/rx-iIa55V4wF-w1200-h1200/Ia55V4wF.jpg"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"128",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                },
+                {
+                    mobilename:"poco c5",
+                    brand:"samsung",
+                    price:"21000",
+                    ram:"6",
+                    Storage:"64",
+                    image:"https://fdn.gsmarena.com/imgroot/news/22/11/poco-c50-india-launch-november/inline/-1200x900m/gsmarena_001.jpg"
+                }
+            ],
+            dummy:{mobilename:"",brand:"",ram:"",storage:"",price:""}        
+        }
+    },
+    methods:{
+        
+        filter()      
+        {
+            this.filt_table=this.Products.filter(x =>  
+                (this.dummy.mobilename ? x.mobilename == this.dummy.mobilename :true)  &&
+                (this.dummy.brand ? x.brand == this.dummy.brand :true)  &&
+                (this.dummy.ram ? x.ram ==this.dummy.ram :true)  &&
+                (this.dummy.storage ? x.Storage==this.dummy.storage :true) &&
+                (this.dummy.price ? x.price===this.dummy.price :true )) 
+            console.log(this.filt_table)   
+        }
+    }
+}
