aee9ce607901a012534dbec2ef5e3ceee3e1d94f
Brundha B <53815867+brundha-it19@users.noreply.github.com>
2023-01-18 19:28:13 +0530

    Add files via upload

---
+++ DAY5/student_detail.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <form>
+            <label for="username">Enter username:</label><br/>
+            <input v-model="student.username" type="text"/><br/>
+            <label for="password">Enter password:</label><br/>
+            <input v-model="student.password" type="text"/><br/>
+            <input type="checkbox"/> Remember Me<br><br><br/>
+            <button @click="verify" id="b1" type="button"> Login </button><br/>
+        </form>
+    </div>
+</template>
+<script src="./js/student.js">
+
+</script>
+
