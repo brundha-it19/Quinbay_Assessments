b7f3832830506c5bdda6f3d9cbe51f328d83ec5a
Brundha B <53815867+brundha-it19@users.noreply.github.com>
2023-01-19 06:09:06 +0530

    Add files via upload

---
+++ DAY5/student.js
@@ -0,0 +1,34 @@
+export default{
+    name:"student_detail",
+    data()           
+    {
+        return{      
+            student:{
+                username:"",
+                password:""
+            },
+            student_detail:[  
+                {username:"Brundha",password:"12345"},
+                {username:"abinaya",password:"12345"},
+                {username:"anu",password:"12345"}
+            ] 
+        }
+    },
+    methods:{
+        verify()
+        {           
+             let val=this.student_detail.find(x =>  
+            this.student.username === x.username  &&
+             this.student.password === x.password);
+            if(val)   
+            {                     
+            alert("login successfull");}
+            else{
+            alert("login failed");}      
+            }
+        }
+    }
+
+
+
+
