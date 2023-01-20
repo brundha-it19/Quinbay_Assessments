0ceed2b8e529aee019638e7211b82be76b0277f7
Brundha B <53815867+brundha-it19@users.noreply.github.com>
2023-01-20 11:28:46 +0530

    Add files via upload

---
+++ DAY7/product_description.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> product description </h1>
+        <h1>{{val().brand}}</h1>
+        <p>{{val().price.priceDisplay}}</p>
+        <p>{{ val().merchantName }}</p>
+        <p>{{ val().itemcount }}</p>
+        <p><img :src="val().images[0]" width="200" height="200"/></p>
+    </div>
+</template>
+<script src="/Users/brundha/Documents/vue/router/src/components/js/product_description.js"> 
+</script>  
+
+
+
+
